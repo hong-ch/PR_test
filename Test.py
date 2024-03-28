4749dc5b55f679e02dfd757c8240504aaf34a391
hong-ch-sub <163513475+hong-ch-sub@users.noreply.github.com>
2024-03-28 14:49:54 +0900

    코드수정(추가) Test.py
    추가로 input을 받아야할 부분이 생각나서 코드를 수정했습니다.

--- Test.py
+++ Test.py
@@ -1,8 +1,8 @@
 # 홍창희, 202204495, 컴퓨터공학전공
 
-name = "Changhee Hong"
-id = 202204495
-major = "Computer Science"
+name = input()
+id = int(input())
+major = intput() # 추가로 수정하고 PR 보냈습니다
 
 print("Hello! I'm", name)
 print("My student id is", id)
