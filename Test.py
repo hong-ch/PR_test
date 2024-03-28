e073a2c700230748f54b42938aa36a3d6f5d3d8f
hong-ch <73926656+hong-ch@users.noreply.github.com>
2024-03-28 14:46:28 +0900

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,9 @@
+# 홍창희, 202204495, 컴퓨터공학전공
+
+name = "Changhee Hong"
+id = 202204495
+major = "Computer Science"
+
+print("Hello! I'm", name)
+print("My student id is", id)
+print("And I'm majoring in", major)
